74a655b9fac9a205341ec22dc0b24dc73ceac761
J <j@Jacobs-MacBook-Pro.local>
2021-02-08 11:40:09 -0800

    initial set up

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1 - environment
+require('dotenv').config();
+
+// 2 - imports
+const express = require('express');
+const axios = require('axios');
+const ejsLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+
+// add controllers
+
+// 3 - App set up
+const app = express();
+app.set('view engine', 'ejs');
+
+// 4 - App Middleware (app.use)
+app.use(ejsLayouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+// 5 - Routes (controllers)
+app.get('/', (req, res) => {
+    res.send('Welcome to my App'); // Yo, Rome: what is this doing?
+});
+
+// TODO set up controllers
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
